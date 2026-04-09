7d7713ad9beb5b51d42cd9c03e1155bb1e27a113
= <uzair952@gmail.com>
2026-04-08 21:38:05 -0400

    Added load function and finished pipeline

--- airflow_etl.py
+++ airflow_etl.py
@@ -1,5 +1,6 @@
 import os
 import requests 
+import sqlite3
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
@@ -70,11 +71,75 @@ def transform(data):
      "author_login": git_author.get("login"),
      "author_id": git_author.get("id"),
      "author_type": git_author.get("type"),
-     "commiter_login": git_committer.get("login"),
-     "commiter_id": git_committer.get("id"),
-     "commiter_type": git_committer.get("type"),
+     "committer_login": git_committer.get("login"),
+     "committer_id": git_committer.get("id"),
+     "committer_type": git_committer.get("type"),
      "html_url": data.get("html_url")
     }
 
+def load(commits):
+
+    #connect to db
+    conn = sqlite3.connect('airflow_commits.db')
+    cursor = conn.cursor()
+
+    #create table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS commits (
+            sha TEXT PRIMARY KEY,
+            message TEXT, 
+            author_name TEXT,
+            author_email TEXT,
+            authored_at TEXT,
+            authored_date TEXT, 
+            committer_name TEXT,
+            committer_email TEXT,
+            committer_at TEXT,
+            committer_date TEXT,
+            verified INTEGER,
+            reason TEXT,
+            author_login TEXT,
+            author_id INTEGER,
+            author_type TEXT,
+            committer_login TEXT,
+            committer_id INTEGER,
+            committer_type TEXT,
+            html_url TEXT
+                   )
+                   ''')
+    
+    print(f"Table Created")
+
+    #batch insert data
+    cursor.executemany('''
+        INSERT OR REPLACE INTO commits (
+            sha, message, author_name, author_email, authored_at, authored_date,
+            committer_name, committer_email, committer_at, committer_date,
+            verified, reason, author_login, author_id, author_type,
+            committer_login, committer_id, committer_type, html_url
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', [(
+        c['sha'], c['message'], c['author_name'], c['author_email'],
+        c['authored_at'], c['authored_date'], c['committer_name'],
+        c['committer_email'], c['committer_at'], c['committer_date'],
+        c['verified'], c['reason'], c['author_login'], c['author_id'],
+        c['author_type'], c['committer_login'], c['committer_id'],
+        c['committer_type'], c['html_url']
+    ) for c in commits])
+
+    print(f"Data Inserted")
+
+    conn.commit()
+    conn.close()
+
+#Call Extract
 extracted_commits = extract()
-print(transform(extracted_commits[0]))
+
+#Call Transform
+records = []
+for commit in extracted_commits:
+    result = transform(commit)
+    records.append(result)
+    
+#Call Load 
+load(records)
